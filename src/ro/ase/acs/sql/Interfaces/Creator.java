c385850d1f1cf5c96718e084328ebb9fd3354a13
CarmenIsaia <116814449+CarmenIsaia@users.noreply.github.com>
2023-03-08 17:12:36 +0200

    Delete src/ro/ase/acs/sql directory

--- src/ro/ase/acs/sql/Interfaces/Creator.java
+++
@@ -1,8 +0,0 @@
-package ro.ase.acs.sql.Interfaces;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface Creator {
-    public  void createTable(Connection connection) throws SQLException;
-}
--- src/ro/ase/acs/sql/Interfaces/Reader.java
+++
@@ -1,8 +0,0 @@
-package ro.ase.acs.sql.Interfaces;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface Reader {
-    public void readData(Connection connection) throws SQLException;
-}
