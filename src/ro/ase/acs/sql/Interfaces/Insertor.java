df9f37c43e8b564cb470723b740a204483c574f9
CarmenIsaia <116814449+CarmenIsaia@users.noreply.github.com>
2023-03-08 17:19:44 +0200

    Delete src/ro/ase/acs/sql directory

--- src/ro/ase/acs/sql/Interfaces/Insertor.java
+++
@@ -1,8 +0,0 @@
-package ro.ase.acs.sql.Interfaces;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface Insertor {
-    public void insertData(Connection connection) throws SQLException;
-}
